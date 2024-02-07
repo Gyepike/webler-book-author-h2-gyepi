ad2af2f92acdf690968582b59ba63bb907731c9a
gyepike <antipet@gmail.com>
2024-02-07 15:35:49 +0100

    Initial Commit

---
+++ src/main/java/hu/webler/weblerbookauthorh2gyepi/WeblerBookAuthorH2GyepiApplication.java
@@ -0,0 +1,13 @@
+package hu.webler.weblerbookauthorh2gyepi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblerBookAuthorH2GyepiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeblerBookAuthorH2GyepiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/webler/weblerbookauthorh2gyepi/WeblerBookAuthorH2GyepiApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.webler.weblerbookauthorh2gyepi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblerBookAuthorH2GyepiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
